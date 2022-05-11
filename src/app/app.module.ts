3bffcebef0f252c5074c74f95355d31f2133a8b7
Bruno <bruno.conrad12@gmail.com>
2022-05-10 22:38:07 -0300

    Adicionado novas rotas e validações

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,6 +13,8 @@ import { HttpClientModule } from '@angular/common/http';
 import { LOCALE_ID } from '@angular/core';
 import { registerLocaleData } from '@angular/common';
 import localePt from '@angular/common/locales/pt';
+import { CurrencyMaskModule } from "ng2-currency-mask";
+import { CorrentistaComponent } from './components/correntista/correntista.component';
 
 registerLocaleData(localePt);
 
@@ -21,13 +23,15 @@ registerLocaleData(localePt);
     AppComponent,
     MovimentacaoListComponent,
     MovimentacaoNewComponent,
+    CorrentistaComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     NgbModule,
     FormsModule,
-    HttpClientModule
+    HttpClientModule,
+    CurrencyMaskModule
   ],
   providers: [{provide: LOCALE_ID, useValue: 'pt-BR'}],
   bootstrap: [AppComponent]
